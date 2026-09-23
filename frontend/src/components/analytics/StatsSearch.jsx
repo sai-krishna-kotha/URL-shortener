57d9fe001e7ff5f5a78495264ffcbbc83eadf26d
Sai Krishna Kotha <kotha.sai.krishna.in@gmail.com>
2026-09-23 18:17:55 +0530

    Make analytics search responsive

--- frontend/src/components/analytics/StatsSearch.jsx
+++ frontend/src/components/analytics/StatsSearch.jsx
@@ -5,9 +5,9 @@ import Button from '../Button';
 
 export default function StatsSearch({ shortCode, setShortCode, isSearching, onSearch }) {
   return (
-    <Card className="p-6 mb-8">
-      <form onSubmit={onSearch} className="flex gap-4 items-end">
-        <div className="flex-1">
+    <Card className="p-4 sm:p-6 mb-6 sm:mb-8">
+      <form onSubmit={onSearch} className="flex flex-col sm:flex-row gap-3 sm:gap-4 sm:items-end">
+        <div className="flex-1 min-w-0">
           <InputField
             label="Enter short code:"
             id="shortCode"
@@ -18,7 +18,7 @@ export default function StatsSearch({ shortCode, setShortCode, isSearching, onSe
             required
           />
         </div>
-        <Button type="submit" disabled={isSearching} className="mb-[2px]">
+        <Button type="submit" disabled={isSearching} className="w-full sm:w-auto whitespace-nowrap">
           <Search className="w-4 h-4 mr-2" />
           {isSearching ? 'Searching...' : 'View analytics'}
         </Button>
